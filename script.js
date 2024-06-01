bdcd398ec717e1be596a4b7b673da264553077fd
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-01 17:12:45 +0430

    change

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-let user_comment = document.getElementById('user-comment');
-let remainingText = document.getElementById('remainingText');
-const maxLength = 70;
+// let user_comment = document.getElementById('user-comment');
+// let remainingText = document.getElementById('remainingText');
+// const maxLength = 70;
 
-function CountRemainingText() {
-    let inputLength = user_comment.value.length;
-    let remaining = maxLength - inputLength;
-    remainingText.textContent = remaining;
+// function CountRemainingText() {
+//     let inputLength = user_comment.value.length;
+//     let remaining = maxLength - inputLength;
+//     remainingText.textContent = remaining;
 
-    if (inputLength >= 50) {
-        remainingText.style.color = 'red';
-        user_comment.style.border = '2px solid red';
-        user_comment.classList.add('textarea-warning');
-    } else {
-        remainingText.style.color = 'black';
-        user_comment.style.backgroundColor = 'white';
-    }
-}
-
-user_comment.addEventListener('input', CountRemainingText);
+//     if (inputLength >= 50) {
+//         remainingText.style.color = 'red';
+//         user_comment.style.border = '2px solid red';
+//         user_comment.classList.add('textarea-warning');
+//     } else {
+//         remainingText.style.color = 'black';
+//         user_comment.style.backgroundColor = 'white';
+//     }
+// }
+// user_comment.addEventListener('input', CountRemainingText);
